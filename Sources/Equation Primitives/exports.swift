b9fb36f86e93375caa8c22725c7d2a7298c567b8
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-07-07 17:50:26 +0200

    style: fix OrderedImports in exports.swift
    - Equation Primitives/exports.swift: sort @_exported imports lexicographically

--- Sources/Equation Primitives/exports.swift
+++ Sources/Equation Primitives/exports.swift
@@ -6,7 +6,7 @@
 // (preserved as a convenience re-export from the pre-migration shape).
 
 @_exported public import Equation_Primitive
+@_exported public import Equation_Primitives_Standard_Library_Integration
 @_exported public import Equation_Protocol_Primitives
 @_exported public import Equation_Tagged_Primitives
-@_exported public import Equation_Primitives_Standard_Library_Integration
 @_exported public import Property_Primitives
